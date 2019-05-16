280740aabbf6472f51e6e64cec88e1baec2ff2ed
Riley Wells <r.wells6894@gmail.com>
2019-05-15 23:29:02 -0400

    Laying Foundation for new Record objects
    So far, have created the classes for Expense and Income records. Instead of dealing with NewIncomeRecord, ExistingIncomeRecord, and SearchResultRecord for dealing with the different stages of record keeping in the program, I am condensing all three possible classes into a single, easy to manage class.
    
    ResultsUtility will now contain almost all of the functions that find and sort records based on the search parameters that the user has specified. For now, I am just trying to copy everything over, more or less, to new files, and then begin to integrate the new classes into the program. Hence the new branch, because things could get messy. While this is a lot of work, this will make the entire project much easier on the eyes and it will make more sense.
    
    tl;dr creating new classes for records and essentially rewriting the searching functions

---
+++ PersonalFinanceProjectFinal/Models/IncomeRecord.cs
@@ -0,0 +1,83 @@
+﻿using PersonalFinanceProjectFinal.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalFinanceProjectFinal.Models
+{
+    class IncomeRecord : Record
+    {
+
+        #region Properties
+        public string OwnerID { get; private set; }
+        private Random Random = new System.Random();
+        public int Modified { get; set; }
+        public string Status { get; set; }
+        public string PreviousStatus { get; set; }
+        public int IsNewRecord { get; set; }
+        #endregion
+
+
+        /// <summary>
+        /// Constructor for an Income Record, regardless if it is new or existing.
+        /// </summary>
+        /// <param name="isNewRecord"></param>
+        /// <param name="ownerId"></param>
+        /// <param name="recordId"></param>
+        /// <param name="amount"></param>
+        /// <param name="date"></param>
+        /// <param name="cat"></param>
+        /// <param name="descript"></param>
+        public IncomeRecord(
+            bool isNewRecord, string ownerId, string recordId, double amount, DateTime date, string cat, 
+            string descript
+            )
+        {
+
+            if (isNewRecord)
+            {
+                OwnerID = ownerId;
+                Hash = Security.getHashSha256(ownerId + amount.ToString() + date.ToString() + Random.Next());
+                Amount = amount;
+                Date = date;
+                Category = cat;
+                Description = descript;
+            }
+            else
+            {
+                Hash = recordId;
+                Amount = amount;
+                Date = date;
+                Category = cat;
+                Description = descript;
+            }
+            Modified = 0;
+            Status = "";
+            PreviousStatus = "";
+            IsNewRecord = 0;
+
+        }
+
+
+        public override bool Equals(object obj)
+        {
+            var temp = obj as IncomeRecord;
+            if (temp != null)
+            {
+                if (!Hash.Equals(temp.Hash))
+                    return false;
+                return Modified == temp.Modified && Amount == temp.Amount && Date.Equals(temp.Date) &&
+                    Category.Equals(temp.Category);
+            }
+            return false;
+        }
+
+
+        public override string ToString()
+        {
+            return $"{Date.ToShortDateString()},\t{Amount},\t{Category},\t{Description}\t\t{Hash}";
+        }
+    }
+}
---
+++ PersonalFinanceProjectFinal/Utilities/ResultsUtility.cs
@@ -0,0 +1,106 @@
+﻿using PersonalFinanceProjectFinal.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace PersonalFinanceProjectFinal.Utilities
+{
+    class ResultsUtility
+    {
+
+
+        public static ObservableCollection<IncomeRecord> GetIncomeResults(List<IncomeRecord> incomeRecords)
+        {
+            ObservableCollection<IncomeRecord> tempRecords = new ObservableCollection<IncomeRecord>();
+            foreach(IncomeRecord item in incomeRecords)
+            {
+                tempRecords.Add(item);
+            }
+            tempRecords.OrderBy(e => e.Date);
+            return tempRecords;
+        }
+
+
+        public static ObservableCollection<ExpenseRecord> GetExpenseResults(List<ExpenseRecord> expenseRecords)
+        {
+            ObservableCollection<ExpenseRecord> tempRecords = new ObservableCollection<ExpenseRecord>();
+            foreach(ExpenseRecord item in expenseRecords)
+            {
+                tempRecords.Add(item);
+            }
+            tempRecords.OrderBy(e => e.Date);
+            return tempRecords;
+        }
+
+
+        /// <summary>
+        /// Determines if the user has entered values for either of the range-related Controls.
+        /// 
+        /// Return 0 - User did not enter values in either control
+        /// Return 1 - User only entered a value in the lower-range Control
+        /// Return 2 - User only entered a value in the higher-range Control
+        /// Return 3 - User entered values in both Controls
+        /// 
+        /// </summary>
+        /// <param name="lowerRange"></param>
+        /// <param name="higherRange"></param>
+        /// <returns></returns>
+        private static int WhatRangeDidUserSelect(Control lowerRange, Control higherRange)
+        {
+            int low = 0;    // 1 = has value, 0 = no value
+            int high = 0;   // 1 = has value, 0 = no value
+
+            if (lowerRange is TextBox && higherRange is TextBox) // controls are TextBoxes
+            {
+                var tempLow = lowerRange as TextBox;
+                var tempHigh = higherRange as TextBox;
+
+                low = tempLow.Text.Equals("MIN") ? 0 : 1;
+                high = tempHigh.Text.Equals("MAX") ? 0 : 1;
+            }
+            else if (lowerRange is DatePicker && higherRange is DatePicker) //  controls are DatePickers
+            {
+                var tempLow = lowerRange as DatePicker;
+                var tempHigh = higherRange as DatePicker;
+
+                low = tempLow.SelectedDate.HasValue ? 1 : 0;
+                high = tempHigh.SelectedDate.HasValue ? 1 : 0;
+            }
+
+            return (low == 1 && high == 1) ? 3 : (low == 0 && high == 0 ? 0 : (low == 1 ? 1 : 2));
+
+        }
+
+
+        public static List<ExpenseRecord> GetExpenseSearchResults(List<ExpenseRecord> expenseRecords,
+            ComboBox combo, TextBox lowAmountRange, TextBox highAmountRange, DatePicker lowDateRange,
+            DatePicker highDateRange)
+        {
+
+            List<ExpenseRecord> tempResultsWithOrWithoutCombo;
+            List<ExpenseRecord> tempResultWithAmountRange;
+            List<ExpenseRecord> tempResultsWithDateRange;
+            List<ExpenseRecord> tempIntermediateResults;
+
+
+            if (combo.Text.Equals("Please select a category..."))
+                tempResultsWithOrWithoutCombo = expenseRecords;
+            else
+                tempResultsWithOrWithoutCombo = expenseRecords.Where(e => e.Category.Equals(combo.Text)).ToList();
+
+
+            switch (WhatRangeDidUserSelect(lowAmountRange, highAmountRange))
+            {
+
+            }
+
+
+            //return tempIntermediateResults;
+
+        }
+    }
+}
