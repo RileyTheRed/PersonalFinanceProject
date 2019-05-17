18778c85af638badb9536f531c46d61f86e160b7
Riley Wells <r.wells6894@gmail.com>
2019-05-16 22:44:48 -0400

    More functions for ResultsUtility.cs
    Created implementations for the GetExpenseSearchResults and GetIncomeSearchResults functions from SearchResults.cs
    
    These implemetations are drastically simplified in comparison because of the revisions with IncomeRecord and ExpenseRecord, instead of multiple types of records for each.
    
    These have not been tested so far though, so I do not know how well, if at all, they work. But they should... We will see.

--- PersonalFinanceProjectFinal/Utilities/ResultsUtility.cs
+++ PersonalFinanceProjectFinal/Utilities/ResultsUtility.cs
@@ -12,7 +12,6 @@ namespace PersonalFinanceProjectFinal.Utilities
     class ResultsUtility
     {
 
-
         public static ObservableCollection<IncomeRecord> GetIncomeResults(List<IncomeRecord> incomeRecords)
         {
             ObservableCollection<IncomeRecord> tempRecords = new ObservableCollection<IncomeRecord>();
@@ -76,13 +75,27 @@ namespace PersonalFinanceProjectFinal.Utilities
         }
 
 
+        /// <summary>
+        /// Takes in specified input parameters that the user selected, list of user expense records.
+        /// 
+        /// Taking into account the options that the user selected, for each of the options a list of
+        /// records is created that adhere to the input parameters. At the end all the list are intersected
+        /// and the result is returned.
+        /// </summary>
+        /// <param name="expenseRecords"></param>
+        /// <param name="combo"></param>
+        /// <param name="lowAmountRange"></param>
+        /// <param name="highAmountRange"></param>
+        /// <param name="lowDateRange"></param>
+        /// <param name="highDateRange"></param>
+        /// <returns></returns>
         public static List<ExpenseRecord> GetExpenseSearchResults(List<ExpenseRecord> expenseRecords,
             ComboBox combo, TextBox lowAmountRange, TextBox highAmountRange, DatePicker lowDateRange,
             DatePicker highDateRange)
         {
 
             List<ExpenseRecord> tempResultsWithOrWithoutCombo;
-            List<ExpenseRecord> tempResultWithAmountRange;
+            List<ExpenseRecord> tempResultsWithAmountRange;
             List<ExpenseRecord> tempResultsWithDateRange;
             List<ExpenseRecord> tempIntermediateResults;
 
@@ -95,12 +108,130 @@ namespace PersonalFinanceProjectFinal.Utilities
 
             switch (WhatRangeDidUserSelect(lowAmountRange, highAmountRange))
             {
+                case 3:
+                    tempResultsWithAmountRange = expenseRecords.Where(e => e.Amount >= Double.Parse(lowAmountRange.Text)
+                        && e.Amount <= Double.Parse(highAmountRange.Text)).ToList();
+                    break;
+
+                case 2:
+                    tempResultsWithAmountRange = expenseRecords.Where(e => e.Amount <= Double.Parse(highAmountRange.Text)).ToList();
+                    break;
+
+                case 1:
+                    tempResultsWithAmountRange = expenseRecords.Where(e => e.Amount >= Double.Parse(lowAmountRange.Text)).ToList();
+                    break;
+
+                default:
+                    tempResultsWithAmountRange = tempResultsWithOrWithoutCombo;
+                    break;
 
             }
 
 
-            //return tempIntermediateResults;
+            switch (WhatRangeDidUserSelect(lowDateRange, highDateRange))
+            {
+                case 3:
+                    tempResultsWithDateRange = expenseRecords.Where(e => e.Date >= lowDateRange.SelectedDate && e.Date <= highDateRange.SelectedDate).ToList();
+                    break;
 
+                case 2:
+                    tempResultsWithDateRange = expenseRecords.Where(e => e.Date <= highDateRange.SelectedDate).ToList();
+                    break;
+
+                case 1:
+                    tempResultsWithDateRange = expenseRecords.Where(e => e.Date >= lowDateRange.SelectedDate).ToList();
+                    break;
+
+                default:
+                    tempResultsWithDateRange = tempResultsWithOrWithoutCombo;
+                    break;
+            }
+
+
+            tempIntermediateResults = tempResultsWithOrWithoutCombo.Intersect(tempResultsWithAmountRange.Intersect(tempResultsWithDateRange)).ToList();
+
+
+            return tempIntermediateResults;
+        }
+
+
+        /// <summary>
+        /// Takes in specified input parameters that the user selected, list of user income records.
+        /// 
+        /// Taking into account the options that the user selected, for each of the options a list of
+        /// records is created that adhere to the input parameters. At the end all the list are intersected
+        /// and the result is returned.
+        /// </summary>
+        /// <param name="incomeRecords"></param>
+        /// <param name="combo"></param>
+        /// <param name="lowAmountRange"></param>
+        /// <param name="highAmountRange"></param>
+        /// <param name="lowDateRange"></param>
+        /// <param name="highDateRange"></param>
+        /// <returns></returns>
+        public static List<ExpenseRecord> GetIncomeSearchResults(List<ExpenseRecord> incomeRecords,
+            ComboBox combo, TextBox lowAmountRange, TextBox highAmountRange, DatePicker lowDateRange,
+            DatePicker highDateRange)
+        {
+
+            List<ExpenseRecord> tempResultsWithOrWithoutCombo;
+            List<ExpenseRecord> tempResultsWithAmountRange;
+            List<ExpenseRecord> tempResultsWithDateRange;
+            List<ExpenseRecord> tempIntermediateResults;
+
+
+            if (combo.Text.Equals("Please select a category..."))
+                tempResultsWithOrWithoutCombo = incomeRecords;
+            else
+                tempResultsWithOrWithoutCombo = incomeRecords.Where(e => e.Category.Equals(combo.Text)).ToList();
+
+
+            switch (WhatRangeDidUserSelect(lowAmountRange, highAmountRange))
+            {
+                case 3:
+                    tempResultsWithAmountRange = incomeRecords.Where(e => e.Amount >= Double.Parse(lowAmountRange.Text)
+                        && e.Amount <= Double.Parse(highAmountRange.Text)).ToList();
+                    break;
+
+                case 2:
+                    tempResultsWithAmountRange = incomeRecords.Where(e => e.Amount <= Double.Parse(highAmountRange.Text)).ToList();
+                    break;
+
+                case 1:
+                    tempResultsWithAmountRange = incomeRecords.Where(e => e.Amount >= Double.Parse(lowAmountRange.Text)).ToList();
+                    break;
+
+                default:
+                    tempResultsWithAmountRange = tempResultsWithOrWithoutCombo;
+                    break;
+
+            }
+
+
+            switch (WhatRangeDidUserSelect(lowDateRange, highDateRange))
+            {
+                case 3:
+                    tempResultsWithDateRange = incomeRecords.Where(e => e.Date >= lowDateRange.SelectedDate && e.Date <= highDateRange.SelectedDate).ToList();
+                    break;
+
+                case 2:
+                    tempResultsWithDateRange = incomeRecords.Where(e => e.Date <= highDateRange.SelectedDate).ToList();
+                    break;
+
+                case 1:
+                    tempResultsWithDateRange = incomeRecords.Where(e => e.Date >= lowDateRange.SelectedDate).ToList();
+                    break;
+
+                default:
+                    tempResultsWithDateRange = tempResultsWithOrWithoutCombo;
+                    break;
+            }
+
+
+            tempIntermediateResults = tempResultsWithOrWithoutCombo.Intersect(tempResultsWithAmountRange.Intersect(tempResultsWithDateRange)).ToList();
+
+
+            return tempIntermediateResults;
         }
     }
 }
